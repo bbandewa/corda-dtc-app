919c89da129c59ddc380d92e4e65fe17f4a771a4
biksen <sen.bikash@gmail.com>
2017-02-12 21:05:54 +0530

    updated

--- digital-trade-chain/src/main/java/com/capgemini/dtc/app/client/DTCClientRPC.java
+++ digital-trade-chain/src/main/java/com/capgemini/dtc/app/client/DTCClientRPC.java
@@ -1,35 +1,38 @@
 package com.capgemini.dtc.app.client;
 
-import com.google.common.net.HostAndPort;
-import com.capgemini.dtc.app.state.DTCState;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import kotlin.Pair;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.node.services.config.ConfigUtilities;
 import net.corda.node.services.messaging.CordaRPCClient;
+
 import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import rx.Observable;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
+import com.capgemini.dtc.app.state.PurchaseOrderState;
+import com.google.common.net.HostAndPort;
 
 /**
- * Demonstration of how to use the CordaRPCClient to connect to a Corda Node and
- * stream some State data from the node.
+ * Demonstration of using the CordaRPCClient to connect to a Corda Node and
+ * steam some State data from the node.
  */
 public class DTCClientRPC {
     public static void main(String[] args) throws ActiveMQException, InterruptedException, ExecutionException {
         if (args.length != 1) {
-            throw new IllegalArgumentException("Usage: TemplateClientRPC <node address>");
+            throw new IllegalArgumentException("Usage: ExampleClientRPC <node address>");
         }
 
         final Logger logger = LoggerFactory.getLogger(DTCClientRPC.class);
         final HostAndPort nodeAddress = HostAndPort.fromString(args[0]);
         final CordaRPCClient client = new CordaRPCClient(nodeAddress, ConfigUtilities.configureTestSSL());
 
-        // Can be amended in the Main file.
+        // Can be amended in the com.example.Main file.
         client.start("user1", "test");
         final CordaRPCOps proxy = client.proxy();
 
@@ -39,13 +42,13 @@ public class DTCClientRPC {
         final List<SignedTransaction> txs = txsAndFutureTxs.getFirst();
         final Observable<SignedTransaction> futureTxs = txsAndFutureTxs.getSecond();
 
-        // Log the existing TemplateStates and listen for new ones.
+        // Log the 'placed' purchase order states and listen for new ones.
         futureTxs.startWith(txs).toBlocking().subscribe(
                 transaction ->
                         transaction.getTx().getOutputs().forEach(
                                 output -> {
-                                    final DTCState templateState = (DTCState) output.getData();
-                                    logger.info(templateState.toString());
+                                    final PurchaseOrderState poState = (PurchaseOrderState) output.getData();
+                                    logger.info(poState.getPurchaseOrder().toString());
                                 })
         );
     }
