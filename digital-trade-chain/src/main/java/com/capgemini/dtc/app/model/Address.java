b542050493388081670ae8117ee71a086e96351c
bbandewa <bbandewar@yahoo.com>
2017-02-20 00:08:21 +0530

    Added data models

---
+++ digital-trade-chain/src/main/java/com/capgemini/dtc/app/model/Address.java
@@ -0,0 +1,66 @@
+package com.capgemini.dtc.app.model;
+
+public class Address {
+
+	String streetName;
+	String streetNumber;
+	String postalCode;
+	String city;
+	String country;
+	
+	/**
+	 * @param streetName
+	 * @param streetNumber
+	 * @param postalCode
+	 * @param city
+	 * @param country
+	 */
+	public Address(String streetName, String streetNumber, String postalCode, String city, String country) {
+		super();
+		this.streetName = streetName;
+		this.streetNumber = streetNumber;
+		this.postalCode = postalCode;
+		this.city = city;
+		this.country = country;
+	}
+	
+	/**
+	 * @return the streetName
+	 */
+	public String getStreetName() {
+		return streetName;
+	}
+	/**
+	 * @return the streetNumber
+	 */
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+	/**
+	 * @return the postalCode
+	 */
+	public String getPostalCode() {
+		return postalCode;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return String.format("Address [streetName=%s, streetNumber=%s, postalCode=%s, city=%s, country=%s]", streetName,
+				streetNumber, postalCode, city, country);
+	}
+	
+}
---
+++ digital-trade-chain/src/main/java/com/capgemini/dtc/app/model/Customer.java
@@ -0,0 +1,75 @@
+package com.capgemini.dtc.app.model;
+
+public class Customer {
+
+	String name;
+	String userName;
+	Address address;
+	String bankName;
+	String VAT;
+	String IBAN;
+	
+	/**
+	 * @param name
+	 * @param userName
+	 * @param address
+	 * @param bankName
+	 * @param vAT
+	 * @param iBAN
+	 */
+	public Customer(String name, String userName, Address address, String bankName, String vAT, String iBAN) {
+		super();
+		this.name = name;
+		this.userName = userName;
+		this.address = address;
+		this.bankName = bankName;
+		VAT = vAT;
+		IBAN = iBAN;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+	/**
+	 * @return the bankName
+	 */
+	public String getBankName() {
+		return bankName;
+	}
+	/**
+	 * @return the vAT
+	 */
+	public String getVAT() {
+		return VAT;
+	}
+	/**
+	 * @return the iBAN
+	 */
+	public String getIBAN() {
+		return IBAN;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return String.format("Customer [name=%s, userName=%s, address=%s, bankName=%s, VAT=%s, IBAN=%s]", name,
+				userName, address, bankName, VAT, IBAN);
+	}
+	
+}
